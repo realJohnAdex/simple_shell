fd7f34eeebccf42de5cc5e07a09a603b5f5d65de
realJohnAdex <johnadex60@gmail.com>
2022-11-10 06:38:49 -0800

    Add shell header file

---
+++ shell.h
@@ -0,0 +1,17 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define TOK_DELIM " \t\r\n"
+
+char *read_line();
+char *_readline();
+char **split_line(char *line);
+int get_input(char *str);
+
+#endif /* SHELL_H */
