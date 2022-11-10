44160c397038412b47df43cdea8e54e0c2460154
realJohnAdex <johnadex60@gmail.com>
2022-11-10 07:29:37 -0800

    Add super simple shell

--- shell.c
+++ shell.c
@@ -9,10 +9,20 @@ int main(void)
 {
 	int nwords = 0;
 	char *in_str;
-	char **arr;
+	char **arr, **argv;
 
 	in_str = _readline();
 	arr = str_split(in_str, &nwords);
+	argv = (char **)malloc(sizeof(char *) * (nwords + 1));
+	argv = arr;
+	argv[nwords] = NULL;
+
+	printf("Before execve\n");
+	if (execve(argv[0], argv, NULL) == -1)
+	{
+		perror("Error:");
+	}
+	printf("After execve\n");
 	for (int i = 0; i < nwords; i++)
 	{
 		printf ("word %d: %s\n", i, arr[i]);
