e5e7cb7db64314bdb7cfaa130b19e5479f04dda0
realJohnAdex <johnadex60@gmail.com>
2022-11-14 01:44:38 -0800

    Add file to print pwd files

---
+++ Exercises/print_dir_files.c
@@ -0,0 +1,23 @@
+#include <dirent.h>
+#include <stdio.h>
+
+/*
+ * main - This program displays the names of all files in the current directory.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	DIR *d;
+	struct dirent *dir;
+	d = opendir(".");
+	if (d) 
+	{
+		while ((dir = readdir(d)) != NULL) 
+		{
+			printf("%s\n", dir->d_name);
+		}
+		closedir(d);
+	}
+	return(0);
+}
