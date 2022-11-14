759d0719455b89538e2533a7b3132f67cdfc2657
realJohnAdex <johnadex60@gmail.com>
2022-11-14 08:06:51 -0800

    Add string manipulation functions

--- shell.h
+++ shell.h
@@ -16,4 +16,13 @@ char *read_line();
 char **split_line(char *line);
 int get_input(char *str);
 
+/* STRING FUNCTIONS */
+int _strlen(const char *s);
+int _strncmp(const char *s1, const char *s2, size_t n);
+char *_strchr(char *s, char c);
+int _strspn(char *s, char *accept);
+char *_strcpy(char *dest, const char *src);
+char *_strcat(char *dest, const char *src);
+char *_strncat(char *dest, const char *src, size_t n);
+
 #endif /* SHELL_H */
