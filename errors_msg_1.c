b4b72d38fbf131a73569e7f9de87461227aa56b6
Dona-j <100310175+Dona-j@users.noreply.github.com>
2022-11-16 21:07:09 +0100

    Revert "Error msg"

--- errors_msg_1.c
+++ errors_msg_1.c
@@ -1,82 +1,10 @@
 #include "shell.h"
 
-char *error_126(char **args);
-char *error_127(char **args);
- 
 char *error_env(char **args);
 char *error_1(char **args);
 char *error_2_exit(char **args);
 char *error_2_cd(char **args);
 char *error_2_syntax(char **args);
-
-/**
- * error_126 - Creates an error message for permission denied failures.
- * @args: An array of arguments passed to the command.
- *
- * Return: The error string.
- */
-char *error_126(char **args)
-{
-	char *error, *hist_str;
-	int len;
-
-	hist_str = _itoa(hist);
-	if (!hist_str)
-		return (NULL);
-
-	len = _strlen(name) + _strlen(hist_str) + _strlen(args[0]) + 24;
-	error = malloc(sizeof(char) * (len + 1));
-	if (!error)
-	{
-		free(hist_str);
-		return (NULL);
-	}
-
-	_strcpy(error, name);
-	_strcat(error, ": ");
-	_strcat(error, hist_str);
-	_strcat(error, ": ");
-	_strcat(error, args[0]);
-	_strcat(error, ": Permission denied\n");
-
-	free(hist_str);
-	return (error);
-}
-
-/**
- * error_127 - Creates an error message for command not found failures.
- * @args: An array of arguments passed to the command.
- *
- * Return: The error string.
- */
-char *error_127(char **args)
-{
-	char *error, *hist_str;
-	int len;
-
-	hist_str = _itoa(hist);
-	if (!hist_str)
-		return (NULL);
-
-	len = _strlen(name) + _strlen(hist_str) + _strlen(args[0]) + 16;
-	error = malloc(sizeof(char) * (len + 1));
-	if (!error)
-	{
-		free(hist_str);
-		return (NULL);
-	}
-
-	_strcpy(error, name);
-	_strcat(error, ": ");
-	_strcat(error, hist_str);
-	_strcat(error, ": ");
-	_strcat(error, args[0]);
-	_strcat(error, ": not found\n");
-
-	free(hist_str);
-	return (error);
-}
-
 /**
  * error_env - Creates an error message for shellby_env errors.
  * @args: An array of arguments passed to the command.
