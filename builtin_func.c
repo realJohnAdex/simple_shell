b78af95eeca0708e889b26975e8280f34b0d810a
Dona-j <udyjimmy6@gmail.com>
2022-11-16 15:58:20 -0800

    file functions reorganized

--- builtin_func.c
+++ builtin_func.c
@@ -6,10 +6,6 @@ void help_cd(void);
 void help_exit(void);
 void help_help(void);
 
-void help_env(void);
-void help_setenv(void);
-void help_unsetenv(void);
-void help_history(void);
 
 /**
  * help_all - Displays all possible builtin shellby commands.
@@ -102,44 +98,5 @@ void help_help(void)
 	msg = "\n      help [BUILTIN NAME]\n\tSee specific information on each ";
 	write(STDOUT_FILENO, msg, _strlen(msg));
 	msg = "builtin command.\n";
-	write(STDOUT_FILENO, msg, _strlen(msg));
-}
-
-/**
- * help_env - Displays information on the shellby builtin command 'env'.
- */
-void help_env(void)
-{
-	char *msg = "env: env\n\tPrints the current environment.\n";
-
-	write(STDOUT_FILENO, msg, _strlen(msg));
-}
-
-/**
- * help_setenv - Displays information on the shellby builtin command 'setenv'.
- */
-void help_setenv(void)
-{
-	char *msg = "setenv: setenv [VARIABLE] [VALUE]\n\tInitializes a new";
-
-	write(STDOUT_FILENO, msg, _strlen(msg));
-	msg = "environment variable, or modifies an existing one.\n\n";
-	write(STDOUT_FILENO, msg, _strlen(msg));
-	msg = "\tUpon failure, prints a message to stderr.\n";
-	write(STDOUT_FILENO, msg, _strlen(msg));
-}
-
-/**
- * help_unsetenv - Displays information on the shellby builtin command
- * 'unsetenv'.
- */
-void help_unsetenv(void)
-{
-	char *msg = "unsetenv: unsetenv [VARIABLE]\n\tRemoves an ";
-
-	write(STDOUT_FILENO, msg, _strlen(msg));
-	msg = "environmental variable.\n\n\tUpon failure, prints a ";
-	write(STDOUT_FILENO, msg, _strlen(msg));
-	msg = "message to stderr.\n";
 	write(STDOUT_FILENO, msg, _strlen(msg));
 }
