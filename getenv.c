9bf7a0ad7ea17e82a97504bc867a8eec3ba6c5cf
realJohnAdex <johnadex60@gmail.com>
2022-11-16 04:21:47 -0800

    Start _setenv function

--- getenv.c
+++ getenv.c
@@ -27,6 +27,33 @@ char *_getenv(const char *name)
 	return (NULL);
 }
 
+/**
+ * _setenv - changes or adds an environment variable
+ * @name: The name of the environmental variable to add or change.
+ * @value: The value of the env variable
+ * @overwrite: then its value is changed to value if
+ * overwrite is nonzero; if overwrite is zero, then the
+ * value of name is not changed
+ *
+ * Return: returns zero on success, or -1 on error.
+ */
+int _setenv(const char *name, const char *value, int overwrite)
+{
+	unsigned int i, len;
+	char *var;
+
+	len = _strlen(name);	
+	for (i = 0; environ[i] != NULL; i++)
+		;
+	if (var = _getenv(name))
+	{
+		if (!overwrite)
+			return (0);
+		//continue here, incomplete function
+	}
+
+}
+
 int main(void)
 {
 	char *var = _getenv("HOME");
